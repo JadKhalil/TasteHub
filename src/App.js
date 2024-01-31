b53fca25bdd749a864702c7d1cad4915b25547f1
Edward <edward.an@ucalgary.ca>
2024-01-31 11:31:22 -0700

    initializing react application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        TASTEHUB
+      </header>
+    </div>
+  );
+}
+
+export default App;
