f1525ee9d268a82eb7b29fa6e813c10017e1e499
JoriDuguid2 <joriduguid2@gmail.com>
2024-02-02 09:52:01 -0700

    Google Login Setup

--- src/App.js
+++ src/App.js
@@ -5,20 +5,31 @@ import Global from "./components/Global.js";
 import Catered from "./components/Catered.js";
 import Profile from "./components/Profile.js";
 import Search from "./components/Search.js";
+import { GoogleLogin } from "@react-oauth/google";
 
 function App() {
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+  const errorMessage = (error) => {
+    console.log(error);
+  };
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="global" element={<Global />}></Route>
-          <Route path="catered" element={<Catered />}></Route>
-          <Route path="profile" element={<Profile />}></Route>
-          <Route path="search" element={<Search />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <br />
+      <br />
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="global" element={<Global />}></Route>
+            <Route path="catered" element={<Catered />}></Route>
+            <Route path="profile" element={<Profile />}></Route>
+            <Route path="search" element={<Search />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import "./index.css"
+/*index.js*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App';
+
+ReactDOM.render(
+    <GoogleOAuthProvider clientId="829294459286-s7kc17ia7gbdu22ut7pe3euo4ncb6ak3.apps.googleusercontent.com">
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </GoogleOAuthProvider>,
+    document.getElementById('root')
 );
