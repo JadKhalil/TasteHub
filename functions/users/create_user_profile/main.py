d48379bf892e4be598752ac912f38ea068ab73bb
Jacob Lever <jacob.lever@ucalgary.ca>
2024-02-11 14:01:32 -0700

    added cloudinary base code(may need modification), installed requests-toolbelt

--- functions/users/create_user_profile/main.py
+++ functions/users/create_user_profile/main.py
@@ -1,7 +1,13 @@
 ##Copied create_comment code to use as boilerplate
 
+from requests_toolbelt.multipart import decoder
 import json
 import boto3
+import requests
+import time
+import hashlib
+import base64
+import os
 
 
 dynamodb_resource = boto3.resource("dynamodb")
@@ -58,3 +64,75 @@ def lambda_handler(event, context):
                     "message":str(exp)
             })
         }
+
+##Cloudinary functionality
+    
+client = boto3.client('ssm')
+
+#get ssm keys
+response = client.get_parameters_by_path(
+    Path='/tastehub/',
+    Recursive=True,
+    WithDecryption=True,
+)
+
+response = {key["Name"]: key["Value"] for key in response["Parameters"]}
+
+#simple function to get keys from Parameter Store
+def get_keys(key_path):
+    return response[key_path]
+
+
+#function to upload file to cloudinary
+'''
+We will call upload_to_cloud() and we will store the result in 'res' if we wish to get the 
+url for the image that needs to be put into DynamoDB we can use: res["secure_url"]
+'''
+
+def upload_to_cloud(filename, resource_type='image'):
+    api_key = "535718123262293" #Jacob's = 535718123262293, Eddie's = 
+    cloud_name = "drua7mqfb" #Jacob's = drua7mqfb, Eddie's = 
+    api_secret = get_keys("/tastehub/cloudinary-key")
+    timestamp = int(time.time())
+
+    body = {
+        "api_key" : api_key,
+        "timestamp" : timestamp
+    }
+
+    files = {
+        "file" : open(filename, "rb")
+    }
+
+    body["signature"] = create_signature(body, api_secret)
+
+    url = f"http://api.cloudinary.com/v1_1/{cloud_name}/{resource_type}/upload/"
+
+    res = requests.post(url, files=files, data=body)
+    
+    return res.json()
+
+#generate signature for cloudinary
+def create_signature(body, api_secret):
+    exclude = ["api_key", "resource_type", "cloud_name"]
+
+    sorted_body = sort_dict(body, exclude)
+    query_str = create_query_string(sorted_body)
+    query_str_appended = f"{query_str}{api_secret}"
+
+    hashed = hashlib.sha1(query_str_appended.encode())
+    signature = hashed.hexdigest()
+
+    return signature
+
+#simple dictionary sorter in alphabetical order
+def sort_dict(dictionary, exclude):
+    return {k: v for k,v in sorted(dictionary.items(), key=lambda item: item[0]) if k not in exclude}
+
+def create_query_string(body):
+    query_string = ""
+
+    for idx, (k, v) in enumerate(body.items()):
+        query_string = f"{k}={v}" if idx == 0 else f"{query_string}&{k}={v}"
+
+    return query_string
