c51edfdbc5e012e87a1d85266f5c023eeb64d585
Edward <edward.an@ucalgary.ca>
2024-02-27 11:05:49 -0700

    created createpost.js page

---
+++ src/components/CreatePost.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
+
+function CreatePostPage() {
+
+    const [email, setEmail] = useState();
+    const [recipeName, setRecipeName] = useState();
+    const [imageFile, setImageFile] = useState();
+    const [prepTime, setPrepTime] = useState();
+    const [description, setDescription] = useState();
+    const [category, setCategory] = useState();
+     
+
+  const handleSubmit = async (e) => { // Add lambda url
+    e.preventDefault(); // Prevents the popup page from closing before event is handled
+    console.log("You clicked submit");
+    const dataToSubmit = new FormData();
+    dataToSubmit.append("userEmail", email);
+    dataToSubmit.append("postID", uuidv4());
+    dataToSubmit.append("recipeName", recipeName);
+    dataToSubmit.append("imageLink", imageFile);
+    dataToSubmit.append("prepTime", prepTime);
+    dataToSubmit.append("postDescription", description);
+    dataToSubmit.append("category", category);
+    dataToSubmit.append("numberOfLikes", 0);
+    dataToSubmit.append("numberOfComments", 0);
+    dataToSubmit.append("datePosted", Date.now());
+    const promise = await fetch(
+        "https://w6twud32h2wkjxtnjdml6vlbq40hrtgx.lambda-url.ca-central-1.on.aws/", // Lambda Function URL (needs to be hard coded)
+        {
+            method: "POST",
+            body: dataToSubmit,
+        }
+    );
+    console.log(dataToSubmit.entries());
+    const jsonPromise = await promise.json(); // Used to access the body of the returned Json
+
+}
+
+  return (
+    <>
+      <div className='popup-container'>
+            <div className='popup-title-container'>
+                <h1>Create a New Post</h1>
+            </div>
+
+            <form onSubmit={ (e) => handleSubmit(e) }>
+                <div className='image-upload-container'>
+                    <label htmlFor='imageUploads' id='imageUploadsLabel'>Select a post image</label>
+                    {imageFile !== null && imageFile !== undefined ? <b> ({imageFile.name})</b>: ""}
+                    <input
+                    type="file" 
+                    id='imageUploads' 
+                    accept='image/*'
+                    onChange={(e) => setImageFile(e.target.files[0])}
+                    required
+                    />
+                </div>
+                <div className='name-container'>
+                    <input className='name-input' type="text" placeholder="Email" onChange={ (e) => setEmail(e.target.value) } required/>
+                </div>
+                <div className='recipeName-container'>
+                    <input className='recipeName-input' type="text" placeholder="Recipe Name" onChange={ (e) => setRecipeName(e.target.value) } required/>
+                </div>
+                <div className='prepTime-container'>
+                    <label>Prep Time</label>
+                    <input className='prepTime-input' min='1' type="number" onChange={ (e) => setPrepTime(e.target.value) } required/>
+                </div>
+                <div className='description-container'>
+                    <input className='description-input' type="text" placeholder="Description" onChange={ (e) => setDescription(e.target.value) } required/>
+                </div>
+                <div className='category-container'>
+                    <input className='category-input' type="text" placeholder="Category" onChange={ (e) => setCategory(e.target.value) } required/>
+                </div>
+                <div className='submit-button-container'>
+                    <input type="submit" id='submit-button' value="Create Post" />
+                </div>
+            </form>
+        </div>
+    </>
+  );
+}
+
+export default CreatePostPage;
--- src/components/Global.js
+++ src/components/Global.js
@@ -1,73 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreatePostPage from "./CreatePost";
 
 function Global() {
-
-
-    // userEmail': userEmail,
-    //                         'bio': bio,
-    //                         'numberOfFollowers': numberOfFollowers,
-    //                         'numberOfFollowing' : numberOfFollowing,
-    //                         'creationDate': creationDate,
-    //                         'image': cloudImage["secure_url"]
-
-  const handleSubmit = async (e) => { // Add lambda url
-    e.preventDefault(); // Prevents the popup page from closing before event is handled
-
-    const dataToSubmit = new FormData();
-    dataToSubmit.append("userEmail", "edward.an03@gmail.com");
-    dataToSubmit.append("bio", "live laugh love");
-    dataToSubmit.append("numberOfFollowers", 0);
-    dataToSubmit.append("numberOfFollowing", 2);
-    const creationDate = Date.now();
-    dataToSubmit.append("creationDate", creationDate);
-    const promise = await fetch(
-        "https://2rg6ktvhkqhy2hlqpekz7u2of40nuvmu.lambda-url.ca-central-1.on.aws/", // Lambda Function URL (needs to be hard coded)
-        {
-            method: "POST",
-            body: dataToSubmit,
-        }
-    );
-    console.log(dataToSubmit.entries());
-    const jsonPromise = await promise.json(); // Used to access the body of the returned Json
-
-    let alreadyExists = false; // Looks for duplicate names in the collection
-    for (let i=0; i<obituaryCollection.length; i++)
-    {
-        if (obituaryCollection[i]['name'] === name)
-        {
-            alreadyExists = true;
-            break;
-        }
-    }
-
-    if (promise.status === 200) // Successful POST request
-    {
-        const newObituary = {
-            'name': name,
-            'birth date': birthDate,
-            'death date': deathDate,
-            'description': jsonPromise['description'],
-            'image': jsonPromise['image'],
-            'speech': jsonPromise['speech'],
-            'submit time': submitTime
-        }
-        setObituaryCollection([newObituary,...obituaryCollection]); // Update obituaryCollection
-        setDescriptionState([true,...descriptionState]); // Most recently added obituary is open by default
-
-        if (alreadyExists === true) { // when an obituary with duplicate name is added
-            window.alert(`There already exists an obituary for ${name}.\nThe old obituary will be deleted upon refresh.`);
-        }
-    }
-    else { // Unsuccessful POST request
-        window.alert(`Error! status ${promise.status}\n${jsonPromise["message"]}`);
-    }
-    setPopupState(false); // Close the overlay
-}
-
   return (
     <>
       <h>Global</h>
+      <CreatePostPage />
     </>
   );
 }
