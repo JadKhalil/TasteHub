679e544cb5841f67afb68ad1e20c34050e3a125b
NoshinZion <noshin.zion@ucalgary.ca>
2024-03-15 02:15:28 -0600

    Added the lambda function for get following posts
    Signed-off-by: NoshinZion <noshin.zion@ucalgary.ca>

---
+++ functions/posts/get_following_posts/main.py
@@ -0,0 +1,67 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+from decimal import Decimal
+
+
+dynamodb_resource = boto3.resource("dynamodb")
+follows_table = dynamodb_resource.Table("tastehub-follows")
+posts_table = dynamodb_resource.Table("tastehub-posts")
+
+# Used to JSON dump data with type Decimal which is not supported by default
+class DecimalEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Decimal):
+      return str(obj)
+    return json.JSONEncoder.default(self, obj)
+
+'''
+documentation not done yet
+
+'''
+
+def lambda_handler(event, context):
+    try:
+        # 1. Get the userEmail from query string parameters
+        queryParameter = event["queryStringParameters"]
+        userEmail = queryParameter["userEmail"]
+
+        # 2. Get a list of users the current user is following
+        following_response = follows_table.query(
+            KeyConditionExpression=Key('userEmailOfFollower').eq(userEmail)
+        )
+        following_emails = [item["userEmailOfFollowee"] for item in following_response["Items"]]
+
+        # 3. If no following users, return empty list
+        if not following_emails:
+            return {
+                "statusCode": 200,
+                "body": json.dumps({
+                    "message": "success",
+                    "postList": [],
+                }, cls=DecimalEncoder)
+            }
+
+        # 4. Construct a filter expression to query for posts from following users
+        filter_expression = Key('userEmail').in_(following_emails)
+
+        # 5. Query the posts table with the filter expression
+        response = posts_table.scan(FilterExpression=filter_expression)
+
+        # 6. Return the list of posts from following users
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "success",
+                "postList": response["Items"],
+            }, cls=DecimalEncoder)
+        }
+
+    except Exception as exp:
+        print(f"exception: {exp}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": str(exp),
+            })
+        }
