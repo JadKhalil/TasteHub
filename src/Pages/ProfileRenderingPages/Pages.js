255ab5b3acbf1638f288cf9b9a000d549d85b06e
JadKhalil <jad.khalil1@ucalgary.ca>
2024-03-16 13:04:17 -0700

    stupid error fix in name

--- src/Pages/ProfileRenderingPages/Pages.js
+++
@@ -1,9 +0,0 @@
-function Pages() {
-    return (
-        <div>
-            Pages
-        </div>
-    )
-}
-
-export default Pages;
---
+++ src/Pages/ProfileRenderingPages/Posts.js
@@ -0,0 +1,9 @@
+function Posts() {
+    return (
+        <div>
+            Posts
+        </div>
+    )
+}
+
+export default Posts;
